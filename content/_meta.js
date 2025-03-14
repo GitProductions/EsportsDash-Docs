691c49ab116ef7d3857d156769ccdf7ab4d4673c
Gitago <76603653+gitagogaming@users.noreply.github.com>
2025-03-14 01:43:32 -0400

    Update _meta.js

--- content/_meta.js
+++ content/_meta.js
@@ -1,31 +1,14 @@
 export default {
-    
     'UserInterface': 'User Interface',
     'API': '',
-
-
-    // 'StreamDeckControls': {
-    //     title: 'Plugin Actions',
-    // },
-
-    // '###': {
-    //     type: 'separator',
-    //     title: 'StreamDeck Plugin' 
-    // },
-
-
-
-
-
+    'StreamDeck': 'Stream Deck Plugin',
     'Support': {
-        'title': 'Support',
-        'type': 'page',
-          
+        title: 'Support',
+        type: 'page',
     },
     'Home': {
         title: 'Home',
         href: 'https://esportsdash.com',
         type: 'page'
-      },
-
+    }
 }
