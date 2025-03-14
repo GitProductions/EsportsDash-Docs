9a4e72c1babce32d9661802de4cc446406849e46
Gitago <76603653+gitagogaming@users.noreply.github.com>
2025-03-13 22:46:40 -0400

    add css

--- app/not-found.js
+++ app/not-found.js
@@ -1,25 +1,10 @@
 import { NotFoundPage } from 'nextra-theme-docs'
+import './styles/notfound.css'
 
 export default function NotFound() {
     return (
-
         <NotFoundPage content="Submit an issue" labels="broken-link">
-            <style jsx>{`
-        @keyframes float {
-          0%, 100% {
-            transform: translateY(0);
-          }
-          50% {
-            transform: translateY(-20px);
-          }
-        }
-      `}</style>
-            <h1
-                style={{
-                    fontSize: '10rem',
-                    animation: 'float 3s ease-in-out infinite',
-                }}
-            >
+            <h1 className="float-animation">
                 404
             </h1>
             <h1>Oops! Page Not Found</h1>
