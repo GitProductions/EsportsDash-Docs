e72bd1f0cc11e196af329c8d82953e093ba589e2
Gitago <76603653+gitagogaming@users.noreply.github.com>
2025-03-15 00:25:57 -0400

    finished with global meta

--- app/_meta.global.js
+++ app/_meta.global.js
@@ -1,29 +1,113 @@
-import { title } from "process";
+// look at the llm repo that uses same library and has a middleware of sorts to give meta data per file in a single location...
+
+
+
 
 export default {
-    index: {
-        title: '🚀 Getting Started',
+
+
+  index: {
+    title: '🚀 Getting Started',
+
     //   title: '✨ Fruits',
     //   items: {
     //     apple: '🍎 Apple',
     //     banana: '🍌 BaNaNa'
     //   }
+  },
+
+  // API Documentation
+  api: {
+    title: 'API',
+    items: {
+
+      'api-introduction': {
+        title: 'Introduction',
+      },
+
+      'api-endpoints': {
+        title: 'Endpoints',
+      },
+
+
+
+    }
+  },
+
+
+  // Stream Deck Docs
+  streamdeck: {
+    title: 'Stream Deck',
+    items: {
+
+      'streamdeck-installation': {
+        title: 'Introduction',
+      },
+
+      'streamdeck-controls': {
+        title: 'Controls',
+      }
     },
+  },
 
-    // API Documentation
-    api: {
-      title: 'API',
 
-      // look at the llm repo that uses same library and has a middleware of sorts to give meta data per file in a single location...
-      items: {
-        'api-introduction': {
-          title: 'Docs - API Introduction | EsportsDash',  
-        },
 
-        'api-endpoints': {
-            title: 'Endpoints',
+
+  // User Interface Docs
+  'user-interface': {
+    title: 'User Interface',
+    items: {
+      'introduction': {
+        title: 'Introduction',
+      },
+
+      'match-tab': {
+        title: 'Match Tab 🚧',
+      },
+      
+      'showinfo-tab': {
+        title: 'Show Info 🚧',
+      },
+
+      'bracket-tab': {
+        title: 'Bracket Tab 🚧',
+      },
+
+      'obs-tab': {
+        title: 'OBS Tab 🚧',
+      },
+
+      'replay-tab': {
+        title: 'Replay Tab 🚧',
+      },
+
+      'teammanager-tab': {
+        title: 'Team Manager 🚧'
         },
 
       }
-    },
-  }
+  },
+
+
+
+  // Top Navigation Menu for Website 
+  website: {
+    title: 'Website',
+    type: 'menu',
+    items: {
+
+      'Home': {
+        title: 'EsportsDash',
+        href: 'https://esportsdash.com',
+      },
+      download: {
+        title: 'Download Esports Dash',
+        href: 'https://esportsdash.com/downloads',
+      },
+      contact: {
+        title: 'Contact Us',
+        href: 'mailto:hi@example.com'
+      },
+    }
+  },
+}
--- content copy/user-interface/match-tab/_meta.js
+++
@@ -1,3 +0,0 @@
-export default {
-    matchtab: 'Match Tab'
-  }
