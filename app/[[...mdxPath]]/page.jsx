b42ed3ffb5a6896694a91fd4aa3a2b29b757aa4c
Gitago <76603653+gitagogaming@users.noreply.github.com>
2025-10-04 19:44:20 -0400

    fix og url with dynamic fix

--- app/[[...mdxPath]]/page.jsx
+++ app/[[...mdxPath]]/page.jsx
@@ -5,7 +5,18 @@ export const generateStaticParams = generateStaticParamsFor('mdxPath')
 
 export async function generateMetadata(props) {
     const params = await props.params
+    
     const { metadata } = await importPage(params.mdxPath)
+
+    // lets dynamically create og path/url for each page based on its url - I see no purpose to hard code this into each post
+    const ogPath = params.mdxPath ? `/${params.mdxPath.join('/')}` : ''
+    if (ogPath) {
+        metadata.openGraph = {
+            ...metadata.openGraph,
+            url: `https://docs.esportsdash.com${ogPath}`
+        }
+    }
+
     return metadata
 }
 
